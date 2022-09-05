77d8cec5a347d8644e8ef37f1e11b2fd633c2a01
Barend Erasmus <hirebarend@gmail.com>
2022-09-05 14:16:06 +0200

    update resume

--- temp/resume.js
+++ temp/resume.js
@@ -3,32 +3,30 @@
 
   const html = await fetch('http://localhost:3000/templates/resume.html');
 
-  const response = await fetch(
-    'http://localhost:8080/api/pdf?async=false',
-    {
-      body: JSON.stringify({
-        blobName: `resume.pdf`,
-        html: await html.text(),
-        options: {
-          // displayHeaderFooter: true,
-          isplayHeaderFooter: false,
-          footerTemplate: `<div style="font-size: 8px; text-align: center; width: 100%;"><i>The content of this document is confidential and intended for ${emailAddress} only.</i></div>`,
-          headerTemplate: '',
-          margin: {
-            bottom: 50,
-            left: 0,
-            right: 0,
-            top: 0,
-          },
-          printBackground: true,
+  const response = await fetch('http://localhost:8080/api/pdf?async=false', {
+    body: JSON.stringify({
+      blobName: `resume.pdf`,
+      html: await html.text(),
+      options: {
+        // displayHeaderFooter: true,
+        isplayHeaderFooter: false,
+        // footerTemplate: `<div style="font-size: 8px; text-align: center; width: 100%;"><i>The content of this document is confidential and intended for ${emailAddress} only.</i></div>`,
+        footerTemplate: '',
+        headerTemplate: '',
+        margin: {
+          bottom: 50,
+          left: 0,
+          right: 0,
+          top: 0,
         },
-      }),
-      headers: {
-        'content-type': 'application/json',
+        printBackground: true,
       },
-      method: 'POST',
-    }
-  );
+    }),
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'POST',
+  });
 
   console.log(await response.json());
 })();
